af6163ed0cac38726c5a7b73b6f265ed7f546d48
Evgenij Gotto <evgenij.gotto@ihsmarkit.com>
2019-07-05 16:27:54 +0300

    Scaffold Identity

---
+++ TestApp.API/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestApp.API.Data;
+
+namespace TestApp.API
+{
+    public class AdminController : Controller
+    {
+		ApplicationDbContext _context;
+		public AdminController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+        public IActionResult Index()
+        {
+			var users = _context.Users.ToList();
+            return View(users);
+        }
+    }
+}
--- TestApp.API/Pages/About.cshtml.cs
+++ TestApp.API/Pages/About.cshtml.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TestApp.API.Data;
 
 namespace TestApp.API.Pages
 {
     public class AboutModel : PageModel
     {
+		ApplicationDbContext _context;
+		public AboutModel(ApplicationDbContext context)
+		{
+			_context = context;
+		}
         public string Message { get; set; }
 
-        public void OnGet()
+		public List<IdentityUser> Users { get; set; }
+
+		public void OnGet()
         {
+			Users = _context.Users.ToList();
             Message = "Your application description page.";
         }
     }
